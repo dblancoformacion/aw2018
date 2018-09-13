b1c5a3523805404090729c4012e6fec7cbda5445
David <dblancoformacion@gmail.com>
2018-09-13 11:06:33 +0200

    Disparadores

--- 20180913_repaso_PLSQL.sql
+++ 20180913_repaso_PLSQL.sql
@@ -1,3 +1,5 @@
+USE provincias;
+
 /* 1) hacer una vista de las provincias de más de un millón de habitantes */
 
 CREATE VIEW provinciotas AS
@@ -29,3 +31,29 @@ CREATE FUNCTION sumaautonomia(vautonomia varchar(255))
     );
   END;
 SELECT sumaautonomia('Andalucia');
+
+/* 4) hacer un disparador que guarde las contraseñas cifradas */
+
+SELECT MD5('');
+CREATE TABLE usuarios(
+  id_usuario int AUTO_INCREMENT,
+  login varchar(127),
+  passwd varchar(255),
+  PRIMARY KEY(id_usuario),
+  UNIQUE(login)
+  );
+INSERT INTO usuarios (login, passwd) VALUES ('Rubén','hola');
+UPDATE usuarios set passwd='hola' WHERE id_usuario=1;
+SELECT * FROM usuarios;
+CREATE TRIGGER cifrador
+  BEFORE INSERT
+  ON usuarios FOR EACH ROW
+  BEGIN
+    SET new.passwd=MD5(new.passwd);
+  END;
+CREATE TRIGGER cifrador2
+  BEFORE UPDATE
+  ON usuarios FOR EACH ROW
+  BEGIN
+    SET new.passwd=MD5(new.passwd);
+  END;
