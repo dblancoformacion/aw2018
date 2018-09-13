b8f3fbe64e23320e2ed5adac779c4e40254ca153
David <dblancoformacion@gmail.com>
2018-09-13 09:29:22 +0200

    Vista y procedimiento

---
+++ 20180913_repaso_PLSQL.sql
@@ -0,0 +1,14 @@
+/* 1) hacer una vista de las provincias de más de un millón de habitantes */
+
+CREATE VIEW provinciotas AS
+  SELECT * FROM provincias WHERE poblacion>1e6;
+SELECT * FROM provinciotas;
+
+/* 2) hacer un procedimiento que sume medio millón de habitantes a las provincias anteriores */
+
+CREATE PROCEDURE sumapoblacion()
+  BEGIN
+    UPDATE provinciotas set poblacion=poblacion+5e5;   
+  END;
+CALL sumapoblacion();
+SELECT * FROM provinciotas;
